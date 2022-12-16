ea72b09891fee9f366155aa2d73454c1d222021a
macaury <carvalhomacaury@gmail.com>
2022-12-16 13:18:51 -0300

    desenvolvimento da tela de ajustes

---
+++ src/teste/telas/testes_telas.java
@@ -0,0 +1,25 @@
+
+import ajustesComponents.OpcoesAcessibilidade;
+import ajustesComponents.teste;
+import telas.Inicio;
+import telas.pop_up_Atencao;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author macaurycarvalho
+ */
+public class testes_telas{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      new teste().setVisible(true);
+    }
+    
+}
